9cbe1343da0b191be3d777a0e078d00a3725d74b
ShadowMuffin512 <liordah0@gmail.com>
2022-06-24 22:20:35 +0300

    overwrite log file

--- src/utils/fileLogger.ts
+++ src/utils/fileLogger.ts
@@ -3,7 +3,9 @@ import winston from 'winston';
 const logger = winston.createLogger({
     transports: [
       new winston.transports.Console(),
-      new winston.transports.File({ filename: 'focuser.log' })
+      new winston.transports.File({ filename: 'focuser.log', options: {
+        flags: 'w'
+      } })
     ]
   });
 
