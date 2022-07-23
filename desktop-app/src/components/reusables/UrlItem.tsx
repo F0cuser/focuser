3c9ae6dd0129da32d0e72cbdc4436e2dedd32221
ShadowMuffin512 <liordah0@gmail.com>
2022-07-23 16:49:30 +0300

    Split url items into components

---
+++ desktop-app/src/components/reusables/UrlItem.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import styles from "./UrlItem.module.css";
+
+import addItemPath from "../../../public/static/images/add-item.svg";
+import deleteItemPath from "../../../public/static/images/delete-item.svg";
+
+const UrlItem = (props: { addItem?: boolean }) => {
+  if (props.addItem) {
+    return (
+      <div className={`${styles.urlItem} ${styles.addUrl} text-center`}>
+        <img src={addItemPath} alt="add-url" class={`${styles.addUrlImage}`} />
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={`${styles.urlItem} mt-3 d-flex align-items-center justify-content-between`}
+      >
+        <p className={`${styles.urlText}`}>google.com</p>
+        <img
+          className={`${styles.urlImage} ${styles.deleteItemImage}`}
+          src={deleteItemPath}
+          alt="delete-url"
+        />
+      </div>
+    );
+  }
+};
+
+export default UrlItem;
--- desktop-app/src/components/routes/urlSelect.tsx
+++ desktop-app/src/components/routes/urlSelect.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styles from './UrlSelect.module.css';
 
-import addItemPath from "../../../public/static/images/add-item.svg";
-import deleteItemPath from "../../../public/static/images/delete-item.svg";
+import UrlItem from "../reusables/UrlItem";
+
+
 
 
 const UrlSelect = () => {
@@ -10,21 +11,9 @@ const UrlSelect = () => {
       <div className={`${styles.urlSelectWrapper} text-center`}>
         <h1 className={`${styles.urlHeader}`}>Website URLs</h1>
         <div className={`${styles.urlListWrapper} d-flex flex-column align-items-center`}>
-          <div className={`${styles.urlItem} ${styles.addUrl} text-center`}>
-            <img
-                  src={addItemPath}
-                  alt="add-url"
-                />
-          </div>
-          <div className={`${styles.urlItem} mt-3 d-flex align-items-center justify-content-between`}>
-            <p className={`${styles.urlText}`}>google.com</p>
-            <img
-                  className={`${styles.urlImage}`}
-                  src={deleteItemPath}
-                  alt="delete-url"
-                />
-          </div>
-          
+          <UrlItem addItem />
+          <UrlItem />
+      
         </div>
       </div>  
     );
