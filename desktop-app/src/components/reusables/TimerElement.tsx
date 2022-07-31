c44516d543929b575dca27547cb4ee749af78023
ShadowMuffin512 <liordah0@gmail.com>
2022-07-31 15:56:21 +0300

    Add element for timer

---
+++ desktop-app/src/components/reusables/TimerElement.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import styles from './TimerElement.module.css';
+
+const TimerElement = () => {
+
+    return (
+        <div className={`${styles.timerElementWrapper}`}>
+            
+        </div>
+    )
+}
+
+export default TimerElement
