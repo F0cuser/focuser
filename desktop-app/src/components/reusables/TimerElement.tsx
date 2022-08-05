29d55a20af8c0544dc9f05682c6756d26c078556
ShadowMuffin512 <liordah0@gmail.com>
2022-08-05 11:21:55 +0300

    Add timer element

--- desktop-app/src/components/reusables/TimerElement.tsx
+++ desktop-app/src/components/reusables/TimerElement.tsx
@@ -1,6 +1,6 @@
-import React , {useRef} from "react";
-import { useDispatch, useSelector } from 'react-redux'
-
+import React, { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useTimer } from "react-timer-hook";
 
 import styles from "./TimerElement.module.css";
 
@@ -10,70 +10,128 @@ import { RootState } from "../../utils/store";
 import { setTimer } from "../../utils/reducers/timer";
 
 const TimerElement = () => {
+  
+  const convertSelectionToDateObj = (timerSelection: {[key:string]: number}): Date => {
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + timerSelection.seconds);
+    time.setMinutes(time.getMinutes() + timerSelection.minutes);
+    time.setHours(time.getHours() + timerSelection.hours);
+    return time;
+  };
+
+
   let timer = useSelector((state: RootState) => state.timer.timer);
+  const timerHook = useTimer({
+    autoStart: false,
+    expiryTimestamp: convertSelectionToDateObj(timer),
+  });
   const dispatch = useDispatch();
   const intervalRef = useRef(0);
 
-
   const onTimerAdjustPress = (digitType: string, timeToAdd: number) => {
-    if (intervalRef.current) return; 
-    intervalRef.current = window.setInterval(() => {updateTime(digitType, timeToAdd)}, 100)
-  }
+    if (intervalRef.current) return;
+    intervalRef.current = window.setInterval(() => {
+      updateTime(digitType, timeToAdd);
+    }, 100);
+  };
 
   const stopTimerAdjust = () => {
     window.clearInterval(intervalRef.current);
-    intervalRef.current = 0
-  }
-
-
+    intervalRef.current = 0;
+  };
 
   const updateTime = (digitType: string, timeToAdd: number) => {
     const prevValue = timer[digitType];
-    if ((prevValue === 0 && timeToAdd < 0) || (digitType !== 'hours' && prevValue === 59 && timeToAdd > 0)) {
-      timer = {...timer, [digitType]: 0}
+    if (
+      (prevValue === 0 && timeToAdd < 0) ||
+      (digitType !== "hours" && prevValue === 59 && timeToAdd > 0)
+    ) {
+      timer = { ...timer, [digitType]: 0 };
+    } else {
+      timer = { ...timer, [digitType]: timer[digitType] + timeToAdd };
     }
-    else {
-      timer = {...timer, [digitType]: timer[digitType] + timeToAdd}
-    }
-    dispatch(setTimer(timer))
-  }
-
+    dispatch(setTimer(timer));
+  };
 
   return (
-    <div className={`${styles.timerElementWrapper} d-flex align-items-center mt-5`}>
+    <div
+      className={`${styles.timerElementWrapper} d-flex align-items-center mt-5`}
+    >
       <div
         className={`d-flex flex-column align-items-center ${styles.segmentWrapper}`}
       >
-        <button className={`${styles.upButton} ${styles.timerAdjust} mb-2`} onMouseUp={stopTimerAdjust} onMouseDown={() => onTimerAdjustPress('hours', 1)}>
+        <button
+          className={`${styles.upButton} ${styles.timerAdjust} mb-2`}
+          onMouseUp={stopTimerAdjust}
+          onMouseDown={() => onTimerAdjustPress("hours", 1)}
+        >
           <img className={`${styles.buttonImage}`} src={upArrowPath} alt="up" />
         </button>
-        <h1 className={`${styles.digits} mt-2`} id="hoursDigits">{timer.hours.toString().padStart(2, '0')}</h1>
-        <button className={`${styles.downButton} ${styles.timerAdjust}`} onMouseUp={stopTimerAdjust} onMouseDown={() => onTimerAdjustPress('hours', -1)}>
-          <img className={`${styles.buttonImage}`} src={downArrowPath} alt="down" />
+        <h1 className={`${styles.digits} mt-2`} id="hoursDigits">
+          {timer.hours.toString().padStart(2, "0")}
+        </h1>
+        <button
+          className={`${styles.downButton} ${styles.timerAdjust}`}
+          onMouseUp={stopTimerAdjust}
+          onMouseDown={() => onTimerAdjustPress("hours", -1)}
+        >
+          <img
+            className={`${styles.buttonImage}`}
+            src={downArrowPath}
+            alt="down"
+          />
         </button>
       </div>
       <div className={`${styles.separator} pb-3`}>:</div>
       <div
         className={`d-flex flex-column align-items-center ${styles.segmentWrapper}`}
       >
-        <button className={`${styles.upButton} ${styles.timerAdjust} mb-2`} onMouseUp={stopTimerAdjust} onMouseDown={() => onTimerAdjustPress('minutes', 1)}>
+        <button
+          className={`${styles.upButton} ${styles.timerAdjust} mb-2`}
+          onMouseUp={stopTimerAdjust}
+          onMouseDown={() => onTimerAdjustPress("minutes", 1)}
+        >
           <img className={`${styles.buttonImage}`} src={upArrowPath} alt="up" />
         </button>
-        <h1 className={`${styles.digits} mt-2`} id="minutesDigits">{timer.minutes.toString().padStart(2, '0')}</h1>
-        <button className={`${styles.downButton} ${styles.timerAdjust}`} onMouseUp={stopTimerAdjust} onMouseDown={() => onTimerAdjustPress('minutes', -1)}>
-          <img className={`${styles.buttonImage}`} src={downArrowPath} alt="down" />
+        <h1 className={`${styles.digits} mt-2`} id="minutesDigits">
+          {timer.minutes.toString().padStart(2, "0")}
+        </h1>
+        <button
+          className={`${styles.downButton} ${styles.timerAdjust}`}
+          onMouseUp={stopTimerAdjust}
+          onMouseDown={() => onTimerAdjustPress("minutes", -1)}
+        >
+          <img
+            className={`${styles.buttonImage}`}
+            src={downArrowPath}
+            alt="down"
+          />
         </button>
       </div>
       <div className={`${styles.separator} pb-3`}>:</div>
       <div
         className={`d-flex flex-column align-items-center ${styles.segmentWrapper}`}
       >
-        <button className={`${styles.upButton} ${styles.timerAdjust} mb-2`} onMouseUp={stopTimerAdjust} onMouseDown={() => onTimerAdjustPress('seconds', 1)}>
+        <button
+          className={`${styles.upButton} ${styles.timerAdjust} mb-2`}
+          onMouseUp={stopTimerAdjust}
+          onMouseDown={() => onTimerAdjustPress("seconds", 1)}
+        >
           <img className={`${styles.buttonImage}`} src={upArrowPath} alt="up" />
         </button>
-        <h1 className={`${styles.digits} mt-2`} id="secondsDigits">{timer.seconds.toString().padStart(2, '0')}</h1>
-        <button className={`${styles.downButton} ${styles.timerAdjust}`} onMouseUp={stopTimerAdjust} onMouseDown={() => onTimerAdjustPress('seconds', -1)}>
-          <img className={`${styles.buttonImage}`} src={downArrowPath} alt="down" />
+        <h1 className={`${styles.digits} mt-2`} id="secondsDigits">
+          {timer.seconds.toString().padStart(2, "0")}
+        </h1>
+        <button
+          className={`${styles.downButton} ${styles.timerAdjust}`}
+          onMouseUp={stopTimerAdjust}
+          onMouseDown={() => onTimerAdjustPress("seconds", -1)}
+        >
+          <img
+            className={`${styles.buttonImage}`}
+            src={downArrowPath}
+            alt="down"
+          />
         </button>
       </div>
     </div>
