cc97e4a77cc9264307644163486d53e3936f3802
ShadowMuffin512 <liordah0@gmail.com>
2022-10-11 12:02:32 +0300

    Add element for deep focus option

--- desktop-app/src/components/reusables/SettingsOption.tsx
+++ desktop-app/src/components/reusables/SettingsOption.tsx
@@ -2,25 +2,26 @@ import React from "react";
 import Switch from "react-switch";
 import styles from "./SettingsOption.module.css";
 
-const SettingsOption = (props: {
-  settingName: string;
-  settingStatus: boolean;
-  handleSwitchChange: (
-    checked: boolean,
-    event: MouseEvent | React.SyntheticEvent<MouseEvent | KeyboardEvent, Event>,
-    id: string,
-  ) => void;
-}) => {
+import { useSelector, useDispatch } from "react-redux";
+import { updateSettings } from "../../utils/reducers/settings";
+import { RootState } from "../../utils/store";
+
+const SettingsOption = (props: { settingName: string, settingLabel: string }) => {
+  const settings = useSelector((state: RootState) => state.settings.settings);
+  const dispatch = useDispatch();
+  const handleSwitchChange = (checked: boolean, _: MouseEvent, id: string) => {
+    dispatch(updateSettings({ newSettings: { [id]: checked } }));
+  };
   return (
     <React.Fragment>
       <label
         className={`${styles.switchWrapper} d-flex align-items-center justify-content-between p-4`}
       >
-        <span className={`${styles.switchLabel}`}>Run on Startup</span>
+        <span className={`${styles.switchLabel}`}>{props.settingLabel}</span>
         <Switch
           id={props.settingName}
-          checked={props.settingStatus}
-          onChange={props.handleSwitchChange}
+          checked={settings[props.settingName]}
+          onChange={handleSwitchChange}
           onColor="#0093d9"
           offColor="#1f0821"
         />
--- desktop-app/src/components/routes/Settings.tsx
+++ desktop-app/src/components/routes/Settings.tsx
@@ -1,23 +1,12 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { updateSettings } from "../../utils/reducers/settings";
-import { RootState } from "../../utils/store";
 import SettingsOption from "../reusables/SettingsOption";
-
-
 import styles from "./Settings.module.css";
 
 const Settings = () => {
-  const settings = useSelector((state: RootState) => state.settings.settings);
-  const dispatch = useDispatch();
-  
-  const handleSwitchChange = (checked: boolean, _: MouseEvent, id: string) => {
-    dispatch(updateSettings({newSettings: {[id]: checked}}));
-  }
-
   return (
     <div className={`${styles.settingsWrapper}`}>
-      <SettingsOption settingName="runOnStartup" settingStatus={settings.runOnStartup} handleSwitchChange={handleSwitchChange} />
+      <SettingsOption settingName="deepFocus" settingLabel="Deep Focus Mode" />
+      <SettingsOption settingName="runOnStartup" settingLabel="Run On Startup"/>
       <h1 className={`${styles.settingsHeader} pageHeader text-center`}>Settings</h1>
 
     </div>
