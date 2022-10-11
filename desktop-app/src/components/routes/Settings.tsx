6279ab35ef63892e41416fd5d8a7a36ce52accb4
ShadowMuffin512 <liordah0@gmail.com>
2022-10-11 11:11:48 +0300

    Refactor setting options into their own component

--- desktop-app/src/components/routes/Settings.tsx
+++ desktop-app/src/components/routes/Settings.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Switch from "react-switch";
 import { updateSettings } from "../../utils/reducers/settings";
 import { RootState } from "../../utils/store";
+import SettingsOption from "../reusables/SettingsOption";
 
 
 import styles from "./Settings.module.css";
@@ -17,10 +17,7 @@ const Settings = () => {
 
   return (
     <div className={`${styles.settingsWrapper}`}>
-        <label className={`${styles.switchWrapper} d-flex align-items-center justify-content-between p-4`}>
-          <span className={`${styles.switchLabel}`}>Run on Startup</span>
-          <Switch id="runOnStartup" checked={settings.runOnStartup} onChange={handleSwitchChange} onColor="#0093d9" offColor="#1f0821"/>
-        </label>
+      <SettingsOption settingName="runOnStartup" settingStatus={settings.runOnStartup} handleSwitchChange={handleSwitchChange} />
       <h1 className={`${styles.settingsHeader} pageHeader text-center`}>Settings</h1>
 
     </div>
