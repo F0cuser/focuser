fade477c707de600c5c9ee41779e2f9a4aaf0e0c
ShadowMuffin512 <liordah0@gmail.com>
2022-10-11 12:02:32 +0300

    Add element for deep focus option

--- desktop-app/src/components/routes/Timer.tsx
+++ desktop-app/src/components/routes/Timer.tsx
@@ -3,7 +3,7 @@ import styles from "./Timer.module.css";
 
 import TimerElement from "../reusables/TimerElement";
 import TimerToggleButton from "../reusables/TimerToggleButton";
-
+import SettingsOption from "../reusables/SettingsOption";
 
 const Timer = (props: {
   updateTime: (arg0: string, arg1: number) => void;
@@ -11,14 +11,17 @@ const Timer = (props: {
 }) => {
   return (
     <div className={`${styles.timerWrapper} text-center`}>
-      <div
-        className={`${styles.mainContent} d-flex justify-content-between align-items-center`}
-      >
-        <TimerElement updateTime={props.updateTime} />
-        <TimerToggleButton
-          toggleFunction={props.toggleTimer}
-          disabled={!new Date()}
-        />
+      <div className={`${styles.mainContent}`}>
+        <div className={`d-flex justify-content-between align-items-center`}>
+          <TimerElement updateTime={props.updateTime} />
+          <TimerToggleButton
+            toggleFunction={props.toggleTimer}
+            disabled={!new Date()}
+          />
+        </div>
+        <div className={`row mt-5 px-3`}>
+          <SettingsOption settingName="deepFocus" settingLabel="Deep Focus Mode" />
+        </div>
       </div>
       <h1 className={`${styles.timerHeader} pageHeader`}>Timer</h1>
     </div>
